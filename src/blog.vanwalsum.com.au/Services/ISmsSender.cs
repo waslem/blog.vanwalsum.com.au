44d43ea6c8f9b9079cf892fb8317a28ec13c7f47
jamie <jamie@JAMIE>
2016-12-26 17:15:25 +0800

    Add project files.

---
+++ src/blog.vanwalsum.com.au/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog.vanwalsum.com.au.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
